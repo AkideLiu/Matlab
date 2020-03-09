f1f39d59e05c53f7857f9d6d3455064059517093
Akide Liu <mail@llycloud.com>
2020-03-09 21:57:22 +1030

    solve tanh(x^2)−1 U1

--- OwnLearning/draw.m
+++ OwnLearning/draw.m
@@ -1,9 +1,13 @@
 clc; clear; close all;
 
-% x = linspace(-10, 10);
-% y = 2.*x.*power(sech(power(x, 2)),2);
-% y1 = tanh(power(x, 2)) -1;
-% plot(x,y1,x,y);
 
-syms x a;
-solve(2*x*sech(x^2)^2,x)
+
+syms x;
+disp(solve(2*x*sech(x^2)^2,x));
+
+x = linspace(-5, 5);
+y = 2.*x.*power(sech(power(x, 2)),2);
+y1 = tanh(power(x, 2)) -1;
+plot(x,y1,x,y);
+grid on;
+grid minor
