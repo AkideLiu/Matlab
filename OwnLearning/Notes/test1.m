5c67baa3ce866a7375b978ba59ab2243cfaedb14
Akide Liu <mail@llycloud.com>
2020-03-10 10:57:45 +1030

    add note for test1.m

--- OwnLearning/Notes/test1.m
+++ OwnLearning/Notes/test1.m
@@ -6,8 +6,10 @@ vec = zeros(5);
 
 for i = 1:5
     vec(i) = input('Enter a number : ');
+    % i is the vector indexes, it's NOT a argument.
     sum = sum + vec(i);
+    % + symbol is increment.
     
 end
 
-fprintf('The mean of the entered numbers is : %f /n ', sum/5);
+fprintf('The mean of the entered numbers is : %f \n ', sum/5);
