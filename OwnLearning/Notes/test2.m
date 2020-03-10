89c6bb20d011a69ae50bccdbb3c6f203246345b3
Akide Liu <mail@llycloud.com>
2020-03-10 10:52:41 +1030

    test 2 init

---
+++ OwnLearning/Notes/test2.m
@@ -0,0 +1,9 @@
+% this program calculates some properties of a circle
+
+
+input('Enter a radius in centimetres : ') = r;
+area = 3.14 * r ^ 2;
+c = 2 * 3.14 * r;
+
+fprintf('A circle with radius %d cm has area %d cm^2', r, area);
+fprintf('and circumference %d cm \n', c );
