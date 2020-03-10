8ee1a9252cbef174666d70d59e39ca5cfd0beaa0
Akide Liu <mail@llycloud.com>
2020-03-10 11:33:13 +1030

    create a random

---
+++ OwnLearning/Notes/Week2/test3.m
@@ -0,0 +1,10 @@
+clc; clear;
+
+for i = 10:-1:1
+    fprintf(' %d ', i );
+end
+fprintf('Blastoff \n');
+
+for i = 100:-2:1
+    fprintf(' %d \n', i );
+end
---
+++ OwnLearning/Notes/Week2/test4.m
@@ -0,0 +1,12 @@
+% script create a vector of 5 random numbers and print the vector's elements 
+vec = [];
+
+for i = 1:5
+    vec(i) = rand() * 100;
+    disp(vec(i));
+end
+
+for i = 1:5
+    vec(i) = randi(100);
+    disp(vec(i));
+end
