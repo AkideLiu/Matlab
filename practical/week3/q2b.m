97b5be0ec2560503e8ee1c6248820016ed1a5dd7
Akide Liu <mail@llycloud.com>
2020-03-14 21:06:12 +1030

    practical 3 Q2b code finish

--- practical/week3/q2b.m
+++ practical/week3/q2b.m
@@ -1,7 +1,7 @@
 clc;
 clear;
 close all;
-% this program will add the elements of B onto the end of A one element at a time.
+% this program will add the elements of B onto the end of A one element at a time in reverse order.
 
 % initialize A and B
 A = [1:10];
@@ -9,10 +9,15 @@ B = [11:20];
 % get A's initial length 
 lengthA = length(A);
 
+% create a new VECTOR to store reserve index of vector B
+reserveIndex = (length(B):-1:1);
+
 % create a for loop , use B's length for times that loop travels
 for i = 1:length(B)
+
     % get every element by index in B and append to A
-    A(lengthA + i) = B(i);
+    A(lengthA + i) = B(reserveIndex(i));
+
 end
 
 % display result 
